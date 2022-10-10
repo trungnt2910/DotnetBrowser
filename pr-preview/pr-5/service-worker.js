cfecad7e46153065fd68ad3a67782edf2920208e
trungnt2910 <trungnt2910@users.noreply.github.com>
2022-10-10 01:51:38 +0000

    Deploy preview for PR 5 🛫

--- pr-preview/pr-5/service-worker.js
+++ pr-preview/pr-5/service-worker.js
@@ -5,11 +5,11 @@ let config = {};
 self.addEventListener('install', function (e) {
     console.debug('[ServiceWorker] Installing offline worker');
     e.waitUntil(
-        fetch("./package_a045726e18288c127e0e64edae68bf12cb8a7e57/uno-config.js")
+        fetch("./package_5a9b0d6a84bac35630cf0afb4bc230ae7cdf7476/uno-config.js")
             .then(r => r.text()
                 .then(configStr => {
                     eval(configStr);
-                    caches.open('package_a045726e18288c127e0e64edae68bf12cb8a7e57').then(function (cache) {
+                    caches.open('package_5a9b0d6a84bac35630cf0afb4bc230ae7cdf7476').then(function (cache) {
                         console.debug('[ServiceWorker] Caching app binaries and content');
                         return cache.addAll(config.offline_files);
                     });
