688d1d8874e500fa53b7d28f295f501c6bc375f8
Trung <57174311+trungnt2910@users.noreply.github.com>
2022-10-14 11:06:33 +0700

    chore: More descriptive website
    Added some more lines to describe how the website is significant and
    guide what users should do with it.
    Also added a few necessary JavaScript methods.

--- sample/Trungnt2910.Browser.Sample/Program.cs
+++ sample/Trungnt2910.Browser.Sample/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Threading.Tasks;
 #if BROWSER
+using System.Linq;
 using System.Runtime.InteropServices.JavaScript;
+using System.Threading.Tasks;
 using Trungnt2910.Browser;
 using Trungnt2910.Browser.Dom;
 #elif WINDOWS
@@ -14,12 +15,39 @@ Console.WriteLine("Hello World!");
 Console.WriteLine(Window.Instance.Location.Href);
 
 var document = JsObject.FromExpression("window.document").Cast<Document>();
-var textNode = document.CreateTextNode("Hello World!");
-document.Body.AppendChild(textNode);
+var header = document.CreateElement("h1");
+header.TextContent = "Hello World!";
+document.Body.AppendChild(header);
+
+var div = document.CreateElement("div");
+div.AppendChild(document.CreateTextNode($"This website is running on "));
+var codeElement = document.CreateElement("code");
+codeElement.TextContent = $"net{Environment.Version.ToString(2)}-browser";
+div.AppendChild(codeElement);
+div.AppendChild(document.CreateTextNode(" target framework."));
+div.AppendChild(document.CreateElement("br"));
+div.AppendChild(document.CreateTextNode("You can build and install the (unofficial) .NET Browser workload "));
+var anchorElement = document.CreateElement("a");
+anchorElement.SetValue("href", "https://github.com/trungnt2910/DotnetBrowser");
+anchorElement.TextContent = "here";
+div.AppendChild(anchorElement);
+div.AppendChild(document.CreateTextNode("."));
+div.AppendChild(document.CreateElement("br"));
+div.AppendChild(document.CreateTextNode("Thanks for visiting my website!"));
+
+document.Body.AppendChild(div);
+
+var para = document.CreateElement("p");
+var em = document.CreateElement("em");
+em.TextContent = "Hint: Try clicking on the bold \"Hello World!\" text 16 times to unlock the page's secret content!";
+
+para.AppendChild(em);
+
+document.Body.AppendChild(para);
 
 int count = 0;
 
-textNode.SelectStart += (sender, jsEvent) =>
+header.SelectStart += (sender, jsEvent) =>
 {
     Console.WriteLine("How dare you select me!");
     ++count;
@@ -44,6 +72,9 @@ document.Paste += (sender, clipboardEvent) =>
     if (!string.IsNullOrEmpty(text))
     {
         Console.WriteLine($"Pasted string: {text}");
+        var jsArr = JsObject.FromExpression("[]").Cast<JsArray>();
+        jsArr.Push(text.Cast<object>().ToArray());
+        Window.Instance["console"].InvokeMember("log", "Pasted characters: ", jsArr);
     }
     else
     {
--- src/Trungnt2910.Browser/Dom/Document.cs
+++ src/Trungnt2910.Browser/Dom/Document.cs
@@ -9,6 +9,8 @@ namespace Trungnt2910.Browser.Dom;
 [JsObjectProperty("body", "Body", nameof(HTMLElement), Comments = "Returns the &lt;body&gt; or &lt;frameset&gt; node of the current document.")]
 [JsObjectReadOnlyProperty("defaultView", "DefaultView", nameof(Window), Comments = "Returns a reference to the <see cref=\"Window.Instance\"/> object.")]
 [JsObjectReadOnlyProperty("location", "Location", nameof(Location), Comments = "Returns the URI of the current document.")]
+[Method<Element, string>("createElement", "CreateElement", Param1 = "tagName", Comments = "Creates a new element with the given tag name.")]
+[Method<Element, string, JsObject>("createElement", "CreateElement", Param1 = "tagName", Param2 = "options", Comments = "Creates a new element with the given tag name.")]
 [Method<Text, string>("createTextNode", "CreateTextNode", Param1 = "data", Comments = "Creates a text node.")]
 [Event<ClipboardEvent>("copy", "Copy", Comments = "The <see cref=\"Copy\"/> event fires when the user initiates a copy action through the browser's user interface.")]
 [Event<ClipboardEvent>("cut", "Cut", Comments = "The <see cref=\"Cut\"/> event fires when the user initiates a cut action through the browser's user interface.")]
