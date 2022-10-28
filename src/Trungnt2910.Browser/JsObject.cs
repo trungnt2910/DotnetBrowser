8ea73a71a427e7a21d98bc0abd7121048cb88974
Trung <57174311+trungnt2910@users.noreply.github.com>
2022-10-28 20:12:05 +0700

    feat(JsObject): New implicit conversions
    Added conversions for supported primitive types to `JsObject`.
    
    This allows more seamless calls to JavaScript functions,
    such as `Window.Instance.Alert("Hello")` instead of
    `Window.Instance.Alert(JsObject.FromExpression("Hello"))`.

--- src/Trungnt2910.Browser/JsObject.cs
+++ src/Trungnt2910.Browser/JsObject.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.JavaScript;
@@ -519,6 +518,123 @@ public partial class JsObject : IConvertible
     }
     #endregion
 
+    #region Conversion from primitives
+    [JSImport($"globalThis.{_jsType}.{nameof(CreateHandle)}")]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    private static partial int CreateHandle(short number);
+
+    [JSImport($"globalThis.{_jsType}.{nameof(CreateHandle)}")]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    private static partial int CreateHandle(int number);
+
+    [JSImport($"globalThis.{_jsType}.{nameof(CreateHandle)}")]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    private static partial int CreateHandle(byte number);
+
+    [JSImport($"globalThis.{_jsType}.{nameof(CreateHandle)}")]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    private static partial int CreateHandle(float number);
+
+    [JSImport($"globalThis.{_jsType}.{nameof(CreateHandle)}")]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    private static partial int CreateHandle(double number);
+
+    [JSImport($"globalThis.{_jsType}.{nameof(CreateHandle)}")]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    private static partial int CreateHandle(string str);
+
+    /// <summary>
+    /// Implicitly converts this <see langword="short"/> value to a <see cref="JsObject"/>.
+    /// </summary>
+    /// <param name="number">The value.</param>
+    [return:NotNullIfNotNull(nameof(number))]
+    public static implicit operator JsObject?(short? number)
+    {
+        if (number == null)
+        {
+            return null;
+        }
+
+        return FromHandle(CreateHandle((short)number));
+    }
+
+    /// <summary>
+    /// Implicitly converts this <see langword="int"/> value to a <see cref="JsObject"/>.
+    /// </summary>
+    /// <param name="number">The value.</param>
+    [return: NotNullIfNotNull(nameof(number))]
+    public static implicit operator JsObject?(int? number)
+    {
+        if (number == null)
+        {
+            return null;
+        }
+
+        return FromHandle(CreateHandle((int)number));
+    }
+
+    /// <summary>
+    /// Implicitly converts this <see langword="byte"/> value to a <see cref="JsObject"/>.
+    /// </summary>
+    /// <param name="number">The value.</param>
+    [return: NotNullIfNotNull(nameof(number))]
+    public static implicit operator JsObject?(byte? number)
+    {
+        if (number == null)
+        {
+            return null;
+        }
+
+        return FromHandle(CreateHandle((byte)number));
+    }
+
+    /// <summary>
+    /// Implicitly converts this <see langword="float"/> value to a <see cref="JsObject"/>.
+    /// </summary>
+    /// <param name="number">The value.</param>
+    [return: NotNullIfNotNull(nameof(number))]
+    public static implicit operator JsObject?(float? number)
+    {
+        if (number == null)
+        {
+            return null;
+        }
+
+        return FromHandle(CreateHandle((float)number));
+    }
+
+    /// <summary>
+    /// Implicitly converts this <see langword="double"/> value to a <see cref="JsObject"/>.
+    /// </summary>
+    /// <param name="number">The value.</param>
+    [return: NotNullIfNotNull(nameof(number))]
+    public static implicit operator JsObject?(double? number)
+    {
+        if (number == null)
+        {
+            return null;
+        }
+
+        return FromHandle(CreateHandle((double)number));
+    }
+
+    /// <summary>
+    /// Implicitly converts this <see langword="string"/> value to a <see cref="JsObject"/>.
+    /// </summary>
+    /// <param name="number">The number.</param>
+    [return: NotNullIfNotNull(nameof(number))]
+    public static implicit operator JsObject?(string? number)
+    {
+        if (number == null)
+        {
+            return null;
+        }
+
+        return FromHandle(CreateHandle((string)number));
+    }
+    #endregion
+
+    #region Equality
     /// <summary>
     /// Compares two JavaScript objects for reference equality.
     /// </summary>
@@ -564,6 +680,7 @@ public partial class JsObject : IConvertible
     {
         return JsHandle;
     }
+    #endregion
 
     /// <summary>
     /// Finalizes the current managed object. This includes
